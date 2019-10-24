9c78e85e9f35784a91f00d40b08cd3fe1ccb2dab
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-24 06:21:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each do |badge_message|
+    
+  end
+end 
